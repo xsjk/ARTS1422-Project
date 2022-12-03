9ed118b9e113a33f912700aa4a42e185d768a3f8
great-luao <you@example.com>
2022-12-03 19:02:26 +0800

    把热力图基本搞差不多了

--- VS1422_PROJECT/src/components/heatmap.vue
+++ VS1422_PROJECT/src/components/heatmap.vue
@@ -9,12 +9,13 @@
 		data(){
 			return{
 				heatmapLayer, 
-				radius_max:0.001,
+				radius_max,
 			}	
 		},
 		methods:{
 			generate_layer(Data){
 					// 配置
+					this.radius_max = 0.015;
 					var cfg = {
 					  'radius': this.radius_max,
 					  'maxOpacity': 0.8,
@@ -31,18 +32,18 @@
 					this.heatmapLayer.setData(Data)
 					return this.heatmapLayer;
 			},
-			async update_layer(scale, date, time, center_arr){
+			async update_layer(date, time, center_arr, scale){
 				var heatmapTest = await out_degree(date, time, center_arr, Math.pow(2,scale-11));
 				var radius = 0;
-				if(scale <= 13){
-					radius = this.radius_max - 0.001*Math.pow(2,scale-11);	
-				}else{
-					radius = 0.001 - 0.0005;
-				}
-				console.log(scale);
+				var days = (date.length!=0 ? date.length:183); 
+				var hours = (time.length!=0 ? time.length:24); 
+				radius = 0.03*Math.pow(2,10-scale);	
+				console.log(days);
+				console.log(hours);
 				console.log(radius);
+				console.log(500*days*hours*Math.pow(2,10-scale));
 				const Data = {
-				  max: heatmapTest.max,
+				  max: 432*days*hours*Math.pow(2,10-scale),
 				  data: heatmapTest
 				};
 				this.heatmapLayer.cfg.radius = radius;
