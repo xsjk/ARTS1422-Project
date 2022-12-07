e95df0aba8cbbb9a00840701cae79efecfd2c618
xsjk <xsjk17@outlook.com>
2022-12-07 23:10:20 +0800

    Revert "Merge branch 'main' of github.com:xsjk/ARTS1422-Project"
    This reverts commit f7f5a2b239aad89915fd7c981b9a4009aa86a461, reversing
    changes made to ebb4ab54ce60c048bad868ee9e36d49a06be300f.

--- VS1422_PROJECT/src/composables/layers/district_division.js
+++ VS1422_PROJECT/src/composables/layers/district_division.js
@@ -18,7 +18,7 @@ export function generate_layer(data, map, s, c) {
 			d > 100  ? '#FC4E2A' :
 			d > 50   ? '#FD8D3C' :
 			d > 20   ? '#FEB24C' :
-			d > 10   ? '#FED976' : 'lightblue';
+			d > 10   ? '#FED976' : '#FFEDA0';
 	}
 
 	function style(feature) {
@@ -31,28 +31,28 @@ export function generate_layer(data, map, s, c) {
 			fillColor: getColor(feature.properties.density)
 		};
 	}
-
+	
 	// control that shows state info on hover
 	const info = L.control.scale({
 		position:'bottomright',
 		maxWidth:'100',
 		imperial:true
 	});
-
+	
 	info.onAdd = function (map) {
 		this._div = L.DomUtil.create('div', 'info');
 		this.update();
 		return this._div;
 	};
-
+	
 	info.update = function (props) {
 		var texts = props ? `<b>${props.name}</b><br />${props.density} 流量` : 'Hover over a state';
 		const contents = texts.bold().fontsize(5);
 		this._div.innerHTML = `${contents}`;
-
+		
 	};
 	info.addTo(map);
-
+	
 	function highlightFeature(e) {
 		const layer = e.target;
 		d3.select(`#${layer.feature.properties['name']} > path`)
@@ -60,23 +60,23 @@ export function generate_layer(data, map, s, c) {
 			.attr('stroke','red')
 
 		layer.setStyle({
-			weight: 2.5,
-			color: 'white',
-			opacity: 0.8,
+			weight: 5,
+			color: '#663408',
+			opacity:0.7,
 			dashArray: '',
-			fillOpacity: 0.4
+			//fillOpacity: 0.7
 		});
 		layer.bringToFront();
 		info.update(layer.feature.properties);
 	}
-
+	
 	function resetHighlight(e) {
 		if(e.target != lastSelection) geojson.resetStyle(e.target);
 		d3.select(`#${e.target.feature.properties['name']} > path`)
 			.attr('stroke-width',0)
 		info.update();
 	}
-
+					
 	function onClick(e) {
 		selected.value = [
 			district_ids[district_names.indexOf(e.target.feature.properties['name'])]
@@ -88,45 +88,8 @@ export function generate_layer(data, map, s, c) {
 			lastSelection = null;
 			return;
 		}
-		if(lastSelection != null){
-			geojson.resetStyle(lastSelection);
-		}
-		// map.flyTo(e.target.getCenter(),10);
-		lastSelection = e.target;
-		lastSelection.setStyle({
-			weight: 2.5,
-			dashArray: '',
-			// fillColor: '#663408',
-			// fillOpacity: 0.7
-			fillOpacity: 0.4
-		});
 	}
-
-	// function onDoubleClick(e) {
-	// 	console.log('double click');
-	// 	selected.value = [
-	// 		district_ids[district_names.indexOf(e.target.feature.properties['name'])]
-	// 	];
-	// 	if (!can_move.value)
-	// 		return;
-	// 	if(lastSelection == e.target){
-	// 		geojson.resetStyle(lastSelection);
-	// 		lastSelection = null;
-	// 		return;
-	// 	}
-	// 	if(lastSelection != null){
-	// 		geojson.resetStyle(lastSelection);
-	// 	}
-	// 	map.flyTo(e.target.getCenter());
-	// 	lastSelection = e.target;
-	// 	lastSelection.setStyle({
-	// 		weight: 10,
-	// 		dashArray: '',
-	// 		fillColor: '#663408',
-	// 		fillOpacity: 0.7
-	// 	});
-	// }
-
+						
 	function onEachFeature(feature, layer) {
 		layer.on({
 			mouseover: highlightFeature,
@@ -143,6 +106,6 @@ export function generate_layer(data, map, s, c) {
 	});
 
 	console.log('geojson',geojson);
-
+	
 	return geojson;
 }
