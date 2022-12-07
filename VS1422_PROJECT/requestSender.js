54a4bda059b09f520ddf036ce82d4c48da9f04e7
Gary Zhang <garyzhang2002@hotmail.com>
2022-12-07 20:07:45 +0800

    update: request sending stuff

--- VS1422_PROJECT/requestSender.js
+++ VS1422_PROJECT/requestSender.js
@@ -1,6 +1,8 @@
+const host_ip = "10.20.101.215"
+
 async function k_min_isochrone_by_departure_time(k, m, d, h) {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/kd', {
+    const response = await axios.get(`http://${host_ip}:5000/kd`, {
     params: {
       k: k,
       m: m,
@@ -16,7 +18,7 @@ async function k_min_isochrone_by_departure_time(k, m, d, h) {
 
 async function k_min_isochrone_by_arrival_time(k, m, d, h) {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/ka', {
+    const response = await axios.get(`http://${host_ip}:5000/ka`, {
     params: {
       k: k,
       m: m,
@@ -32,7 +34,7 @@ async function k_min_isochrone_by_arrival_time(k, m, d, h) {
 
 async function origin(d, h, m) {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/o', {
+    const response = await axios.get(`http://${host_ip}:5000/o`, {
     params: {
       d: d,
       h: h,
@@ -47,7 +49,7 @@ async function origin(d, h, m) {
 
 async function dest(d, h, m) {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/d', {
+    const response = await axios.get(`http://${host_ip}:5000/d`, {
     params: {
       d: d,
       h: h,
@@ -62,7 +64,7 @@ async function dest(d, h, m) {
 
 async function out_degree(d, h, m, e) {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/od', {
+    const response = await axios.get(`http://${host_ip}:5000/od`, {
     params: {
       d: d,
       h: h,
@@ -78,7 +80,7 @@ async function out_degree(d, h, m, e) {
 
 async function in_degree(d, h, m, e) {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/id', {
+    const response = await axios.get(`http://${host_ip}:5000/id`, {
     params: {
       d: d,
       h: h,
@@ -94,7 +96,7 @@ async function in_degree(d, h, m, e) {
 
 async function traffic_flow_in_degree_graph(t) {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/tfi', {
+    const response = await axios.get(`http://${host_ip}:5000/tfi`, {
     params: {
       t: t
     }
@@ -107,7 +109,7 @@ async function traffic_flow_in_degree_graph(t) {
 
 async function traffic_flow_out_degree_graph(t) {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/tfo', {
+    const response = await axios.get(`http://${host_ip}:5000/tfo`, {
     params: {
       t: t
     }
@@ -120,7 +122,7 @@ async function traffic_flow_out_degree_graph(t) {
 
 async function draw_topological_graph_by_departure_time(d, h) {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/drwd', {
+    const response = await axios.get(`http://${host_ip}:5000/drwd`, {
     params: {
       d: d,
       h: h
@@ -134,7 +136,7 @@ async function draw_topological_graph_by_departure_time(d, h) {
 
 async function draw_topological_graph_by_arrival_time(d, h) {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/drwa', {
+    const response = await axios.get(`http://${host_ip}:5000/drwa`, {
     params: {
       d: d,
       h: h
